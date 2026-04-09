3adb1ec78a354b562002eca724a24f01eaed3f01
Juanjo1505 <jurregojo@cesde.net>
2026-04-09 18:47:02 -0500

    Se crea el proyecto en intellij

---
+++ src/main/java/com/example/NewCharper/NewCharperApplication.java
@@ -0,0 +1,13 @@
+package com.example.NewCharper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCharperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewCharperApplication.class, args);
+	}
+
+}
